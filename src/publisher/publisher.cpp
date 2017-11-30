29cf10469f075c9160294dc30c4e6f330b7cb813
baxter <atp@ccs.neu.edu>
2017-11-30 17:04:08 -0500

    first commit

---
+++ src/publisher/publisher.cpp
@@ -0,0 +1,52 @@
+#include <pcl/point_types.h>
+#include <ros/ros.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+#include "../cluster/cluster_extraction.cpp"
+
+
+typedef pcl::PointCloud<pcl::PointXYZRGB> PointCloud;
+
+PointCloud::Ptr base (new PointCloud);
+
+void callback(const PointCloud::ConstPtr& msg)
+{
+  printf ("Cloud: width = %d, height = %d\n", msg->width, msg->height);
+  if (base->points.size() == 0)
+  {
+    //pcl::copyPointCloud(msg, *base);
+    base = msg->makeShared(); 
+  }
+}
+
+
+int main(int argc, char** argv)
+{
+  ros::init (argc, argv, "sub_cloud_base");
+  ros::NodeHandle nh;
+  ros::Rate loop_rate(4);
+    
+    
+  ros::Subscriber sub = nh.subscribe<pcl::PointCloud<pcl::PointXYZRGB>>("/cloud_base", 1, callback);
+  while (base->points.size() == 0){
+    ros::spinOnce ();
+    loop_rate.sleep ();
+  }
+  
+  
+  ros::init (argc, argv, "pub_pcl");
+  ros::Publisher pub = nh.advertise<PointCloud> ("points2", 1);
+  
+  std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> targets = getCluster(base);
+  PointCloud::Ptr msg = targets.at(1);
+  
+
+  while (nh.ok())
+  {
+    msg->header.stamp = ros::Time::now().toNSec();
+    pub.publish (msg);
+    ros::spinOnce ();
+    loop_rate.sleep ();
+  }
+  return 0;
+}
